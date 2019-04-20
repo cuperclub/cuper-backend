64bedf0a05f1ff7b455970660c6073fc9ed5b72d
Lenin Capa <leviskp87@gmail.com>
2019-04-19 19:09:04 -0500

    add columns to category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,9 +2,11 @@
 #
 # Table name: categories
 #
-#  id         :bigint(8)        not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                :bigint(8)        not null, primary key
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  name              :string
+#  points_per_dollar :integer          default(1)
 #
 
 require 'rails_helper'
