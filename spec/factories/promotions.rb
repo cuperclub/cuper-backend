67a9febf2ebe560031a45b1b68ce90e1f0acf015
Darwin Gualotuña <daosgc@gmail.com>
2019-05-05 12:36:59 -0500

    rename some columns into promotion model

--- spec/factories/promotions.rb
+++ spec/factories/promotions.rb
@@ -2,20 +2,20 @@
 #
 # Table name: promotions
 #
-#  id             :bigint(8)        not null, primary key
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  title          :string
-#  description    :string
-#  terms          :string
-#  image          :string
-#  background     :string
-#  totalRewards   :integer          default(0)
-#  pointsRequired :integer          default(0)
-#  startAt        :datetime
-#  endAt          :datetime
-#  unlimited      :boolean          default(FALSE)
-#  office_id      :bigint(8)
+#  id              :bigint(8)        not null, primary key
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  title           :string
+#  description     :string
+#  terms           :string
+#  image           :string
+#  background      :string
+#  total_rewards   :integer          default(0)
+#  points_required :integer          default(0)
+#  start_at        :datetime
+#  end_at          :datetime
+#  unlimited       :boolean          default(FALSE)
+#  office_id       :bigint(8)
 #
 
 FactoryBot.define do
